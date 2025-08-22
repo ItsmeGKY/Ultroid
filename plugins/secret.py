1dd5f9b5ea27360f55445e5a87006e87851e9b04
Gokul G <80095316+ItsmeGKY@users.noreply.github.com>
2025-08-22 16:35:45 +0300

    Update secret.py

--- plugins/secret.py
+++ plugins/secret.py
@@ -4,15 +4,11 @@
 
 import os
 import time
-from datetime import datetime as dt
 
 from telethon.errors.rpcerrorlist import MessageNotModifiedError
 
 from . import (
     ULTConfig,
-    eor,
-    progress,
-    time_formatter,
     ultroid_cmd,
 )
 
@@ -37,17 +33,11 @@ async def secret_download(event):
     os.makedirs(download_path, exist_ok=True)
 
     # --- Download ---
-    start = time.time()
     try:
         file_name = await event.client.download_media(
             reply,
             download_path,
-            progress_callback=lambda d, t: event.client.loop.create_task(
-                progress(d, t, event, start, "Downloading...")
-            ),
         )
-    except MessageNotModifiedError:
-        return
     except Exception as err:
         return await event.client.send_message(
             ULTConfig.LOG_CHAT, f"❌ **Secret Plugin Error (Download)**:\n`{err}`"
